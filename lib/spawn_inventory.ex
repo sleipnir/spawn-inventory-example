6c38f76d91636a463bda65d58563143de7a0be38
Adriano Santos <sleipnir@bsd.com.br>
2023-03-08 12:08:51 -0300

    Initial project structure

---
+++ lib/spawn_inventory.ex
@@ -0,0 +1,18 @@
+defmodule SpawnInventory do
+  @moduledoc """
+  Documentation for `SpawnInventory`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SpawnInventory.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
